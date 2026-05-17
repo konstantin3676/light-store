b152c5bccc07e22eba91be9fa9d02774e2ff9600
Konstantin <93384228+konstantin3676@users.noreply.github.com>
2026-05-17 15:31:28 +0300

    Add logout

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router';
 
-import { Anchor } from '@mantine/core';
+import { ActionIcon, Anchor } from '@mantine/core';
+import { SignOutIcon } from '@phosphor-icons/react';
 
+import { useAppDispatch, useAppSelector } from '../../hook';
+import {
+  getUserAuthData,
+  getUserInited,
+} from '../../slices/userSlice/selectors';
+import { userActions } from '../../slices/userSlice/userSlice';
 import { BasketButton } from '../BasketButton/BasketButton';
 import classes from './Header.module.css';
 
 export const Header = () => {
+  const dispatch = useAppDispatch();
+  const authData = useAppSelector(getUserAuthData);
+  const inited = useAppSelector(getUserInited);
+
   return (
     <header className={classes.header}>
       <Anchor
@@ -16,7 +27,20 @@ export const Header = () => {
       >
         Магазин лампочек
       </Anchor>
-      <BasketButton />
+      <div className={classes.buttonContainer}>
+        <BasketButton />
+        {inited && authData?.access_token && (
+          <ActionIcon
+            variant="outline"
+            size="md"
+            onClick={() => {
+              dispatch(userActions.logout());
+            }}
+          >
+            <SignOutIcon style={{ width: '70%', height: '70%' }} />
+          </ActionIcon>
+        )}
+      </div>
     </header>
   );
 };
